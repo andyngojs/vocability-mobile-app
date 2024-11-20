a0e913396e443e282e14d4a5a75e09f40202e4a3
dongnb <dongnbas@gmail.com>
2024-11-21 03:24:22 +0700

    chore: add ui for collection screen

--- src/screens/Collection/Collection.tsx
+++ src/screens/Collection/Collection.tsx
@@ -1,13 +1,74 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useCallback} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {appConfig} from '@src/app';
+import {Icon, AppModal} from '@src/components';
 
 const _Collection = () => {
-  return (
-    <View>
-      <Text>Collection screen</Text>
-    </View>
-  )
-}
+  const [isVisible, setIsVisible] = React.useState(false);
 
-const Collection = React.memo(_Collection)
-export default Collection
+  const handleShowModalCreateCollection = useCallback(() => {
+    setIsVisible(true);
+  }, []);
+
+  const handleHideModalCreateCollection = useCallback(() => {
+    setIsVisible(false);
+  }, []);
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.titleGreeting}>Welcome back!</Text>
+
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={styles.buttonContainer}
+          onPress={handleShowModalCreateCollection}>
+          <Icon name={'add-circle'} size={18} color="#fff" />
+          <Text style={{fontSize: 14, color: '#fff'}}>Create collection</Text>
+        </TouchableOpacity>
+      </View>
+
+      <AppModal
+        position={'center'}
+        isVisible={isVisible}
+        onClose={handleHideModalCreateCollection}
+        title={'Create collection'}>
+        <Text>Hhello ddjdj</Text>
+        <Text>Hhello ddjdj</Text>
+        <Text>Hhello ddjdj</Text>
+        <Text>Hhello ddjdj</Text>
+
+      </AppModal>
+    </SafeAreaView>
+  );
+};
+
+const Collection = React.memo(_Collection);
+export default Collection;
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    backgroundColor: '#000',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+  },
+  titleGreeting: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  buttonContainer: {
+    borderColor: '#fff',
+    borderWidth: 1,
+    borderRadius: 18,
+    paddingHorizontal: 16,
+    paddingVertical: 14,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%',
+    backgroundColor: '#888',
+    marginTop: 24,
+  },
+});
